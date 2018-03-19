9b646a08c16ed6116e3a38bc1e3ccdf331e528b2
frankHelens <798356548@qq.com>
2018-03-19 08:51:25 +0800

    入库单页面修改

--- src/containers/Manage/EnterStock/Detail.vue
+++ src/containers/Manage/EnterStock/Detail.vue
@@ -7,6 +7,7 @@
     :tableData="tableData"
     :columns="columns"
     :values="values"
+    :resource="resource"
     :submitResource="submitResource"
     :formTableColumns="formTableColumns")
 </template>
@@ -23,10 +24,11 @@ export default {
     return {
       formList: ['origin', 'storageType', 'enterPrice', 'takePe', 'makePe', 'remark'],
       tableList: ['code', 'productName', 'unit', 'enterNum', 'unitPrice', 'productPrice', 'remark'],
-      tableSubmitList: ['code', 'productId', 'enterNum', 'productPrice', 'remark'],
+      tableSubmitList: ['code', 'productId', 'enterNum', 'unitPrice', 'productPrice', 'remark'],
       columns: formColumns,
       formTableColumns: formTableColumns,
       values: {},
+      resource: 'enterStock/detail',
       submitResource: 'enterStock/detail',
       tableData: [{
         id: 0
@@ -40,6 +42,14 @@ export default {
         id: 4
       }, {
         id: 5
+      }, {
+        id: 6
+      }, {
+        id: 7
+      }, {
+        id: 8
+      }, {
+        id: 9
       }],
       productOptions: [],
       loading: false
